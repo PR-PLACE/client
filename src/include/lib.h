c47473228f478489d71856c411e3582a3aceeb0a
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2022-11-29 15:16:48 +0100

    lib.h

--- src/include/lib.h
+++ src/include/lib.h
@@ -1,7 +1,20 @@
 #include <sys/shm.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define PROJECT_ID 10
 
-#define KEY_FILE_NAME "key_file"
+#define KEY_FILE_NAME "pr-place-shm"
 
 #define SHM_FLAG 0666 | IPC_CREAT | IPC_EXCL
+
+#define COLUMNS_NUMBER 50
+#define ROWS_NUMBER 50
+
+#define MAP_FILENAME "pr-place-map"
+
+typedef struct client_list_t
+{
+    int nb_client;
+    pid_t *client_list;
+} client_list_t;
